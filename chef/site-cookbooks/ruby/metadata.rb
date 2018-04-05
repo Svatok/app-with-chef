dec9d7fa47d387680a0b683c211371d0eccc0d10
Svatok <s_s_s@ua.fm>
2018-04-05 22:39:49 +0300

    set only users

--- chef/site-cookbooks/ruby/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'ruby'
-maintainer       'Dmitriy Grechukha'
-maintainer_email 'dmitriy.grechukha@gmail.com'
-license          'All rights reserved'
-description      'Ruby configuration'
-long_description 'Ruby configuration'
-version          '0.1.0'
-
-depends 'ruby_build'
