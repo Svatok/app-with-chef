af18415e36f0b6d8c8b0f050773bebee608e0c9e
Svatok <s_s_s@ua.fm>
2018-04-05 23:47:17 +0300

    add nginx

---
+++ chef/site-cookbooks/nginx-config/metadata.rb
@@ -0,0 +1,9 @@
+name             'nginx-config'
+maintainer       'Svatok'
+maintainer_email 's_s_s@ua.fm'
+license          'All rights reserved'
+description      'Installs/Configures nginx-config'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'nginx'
