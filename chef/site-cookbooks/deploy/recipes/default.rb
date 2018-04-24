4da0fad0ea0008cc04490b23d7525e6ef28a284f
Svatok <s_s_s@ua.fm>
2018-04-25 02:52:31 +0300

    add puma file

--- chef/site-cookbooks/deploy/recipes/default.rb
+++ chef/site-cookbooks/deploy/recipes/default.rb
@@ -57,6 +57,16 @@ template File.join(config_path, 'database.yml') do
   mode 0o644
 end
 
+template File.join(shared_path, 'puma.rb') do
+  source File.join(node.environment, 'puma.rb.erb')
+  variables(
+    environment: node.environment,
+    project_root: root_path
+  )
+  owner deployer
+  group deployer
+  mode 0o644
+end
 
 # rubocop:disable Metrics/BlockLength
 timestamped_deploy node['app_name'] do
@@ -85,10 +95,17 @@ timestamped_deploy node['app_name'] do
   )
 
   symlink_before_migrate(
+    # 'config/secrets.yml.key' => 'config/secrets.yml.key',
     'config/database.yml' => 'config/database.yml'
   )
 
   before_migrate do
+    # file maintenance_file do
+    #   owner deployer
+    #   group deployer
+    #   action :create
+    # end
+
     execute 'install bundler' do
       command "/bin/bash -lc 'gem install bundler'"
       cwd release_path
